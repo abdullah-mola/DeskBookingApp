aa8ea3f5fa79a90489861e9dc7c4fdb9d966ab8c
Abdullah Mola <Abdullah.MolaHussain@edu.fh-kaernten.ac.at>
2022-02-24 14:27:28 +0100

    add Tabbar

--- app/src/main/java/com/example/deskbookingappllication/ui/MainActivity.kt
+++ app/src/main/java/com/example/deskbookingappllication/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.deskbookingappllication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.deskbookingappllication.R
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -18,33 +17,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpTabBar()
+//        setUpTabBar()
 
 
     }
 
-    private fun setUpTabBar()
-    {
-        val adapter = TabPageAdapter(this, tabLayout.tabCount)
-        viewPager.adapter = adapter
-
-        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
-        {
-            override fun onPageSelected(position: Int) {
-                tabLayout.selectTab(tabLayout.getTabAt(position))
-            }
-        })
-
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
-        {
-            override fun onTabSelected(tab: TabLayout.Tab)
-            {
-                viewPager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
-    }
+//    private fun setUpTabBar()
+//    {
+//        val adapter = TabPageAdapter(this, tabLayout.tabCount)
+////        viewPager.adapter = adapter
+//
+////        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
+//        {
+//            override fun onPageSelected(position: Int) {
+//                tabLayout.selectTab(tabLayout.getTabAt(position))
+//            }
+//        })
+//
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
+//        {
+//            override fun onTabSelected(tab: TabLayout.Tab)
+//            {
+//                viewPager.currentItem = tab.position
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {}
+//        })
+//    }
 }
